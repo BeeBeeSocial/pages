9a2a1807337f60a599de57a1b14236a1417c3ee5
Joseph Hall <joehall@moreoncode.com>
2022-08-21 22:30:59 -0400

    First draft of site; missing API server

---
+++ build-all.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+echo "============================"
+echo "Building the application ..."
+echo "============================"
+echo " "
+
+pushd ../../site/beebee.web.spa/ClientApp/
+npm run build
+popd
+
+echo " "
+echo "====================="
+echo "Building the blog ..."
+echo "====================="
+echo " "
+
+pushd blog/
+bundle exec jekyll build
+popd
+
+echo " "
+echo "============================="
+echo "Building the help content ..."
+echo "============================="
+echo " "
+
+pushd help/
+bundle exec jekyll build
+popd
+
+echo " "
+echo "====================="
+echo "Cleaning docs/ folder"
+echo "====================="
+echo " "
+
+rm -rf docs/
+mkdir docs/
+
+echo " "
+echo "============================="
+echo "Copying static files to docs/"
+echo "============================="
+echo " "
+
+pushd docs/
+cp -R ../../site/beebee.web.spa/ClientApp/build/ ./
+mkdir blog/
+cp -R ../blog/_site/ blog/
+mkdir help/
+cp -R ../help/_site/ help/
+popd
+
+echo " "
+echo "Done."
+echo " "
